d10614fd06b657744782c5347a66cebd2dd5de6e
my_givenchy <931779265@qq.com>
2018-12-04 21:05:48 +0800

    save version

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 
 def config():
     cfg = {
-        'epochs' : 3000,
+        'epochs' : 10000,
         'LOG_INTERVAL' : 200,
         'content_filepath' : './content',
         'style_filepath' : './style',
+        'content_batch_size' : 5,
         'style_size' : 5,
+        'img_size' : 64,
         'debug' : 0,
-        'lr' : 4*1e-7,
+        'lr' : 4*1e-5,
         'momentum' : 0.3,
         'T' : 2,
         'lambda' : 1,
